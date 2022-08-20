5645c6af0dd628bbc6b0d0b0b92009ccd79456a7
juanpatinoch <juanpatinoch@gmail.com>
2022-08-20 18:54:52 -0500

    Se agrega el viewmodel de home

--- app/src/main/java/com/cinema/emovie/ui/home/view/HomeActivity.kt
+++ app/src/main/java/com/cinema/emovie/ui/home/view/HomeActivity.kt
@@ -1,14 +1,18 @@
 package com.cinema.emovie.ui.home.view
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.cinema.emovie.databinding.ActivityHomeBinding
+import com.cinema.emovie.ui.home.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
