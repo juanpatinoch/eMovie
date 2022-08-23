fda4b4ff2fc6cdb2ff3efb1e05c65a4a70a9cbac
juanpatinoch <juanpatinoch@gmail.com>
2022-08-23 00:18:06 -0500

    Se agrega diseno custom para radiobutton group

--- app/src/main/java/com/cinema/emovie/ui/home/view/HomeActivity.kt
+++ app/src/main/java/com/cinema/emovie/ui/home/view/HomeActivity.kt
@@ -22,6 +22,7 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setupBinding()
         subscribeObserver()
         getInitData()
